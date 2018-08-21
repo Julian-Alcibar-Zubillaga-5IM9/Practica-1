d74650f43be41d1f31d59b00ee995ddd04708a91
Julian-Alcibar-Zubillaga-5IM9 <42345442+Julian-Alcibar-Zubillaga-5IM9@users.noreply.github.com>
2018-08-20 19:01:55 -0500

    Add files via upload
    Hola-Mundo-Julian-Alcibar-5IM9

---
+++ Hola-Mundo/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hola Mundo");
+        for (int i = 0; i < 10; i++){
+            System.out.println("Julian Alcibar Zubillaga-5IM9");
+        }
+    }
+}
